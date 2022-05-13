746d7134f8095f84f8910dca2f5fed1b5f8421bf
MyUsername <romamargine@gmail.com>
2022-05-13 16:43:48 +0200

    Add font awesome and create scss folder with style and variables

--- modern-schooling/src/main.js
+++ modern-schooling/src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* FONT AWESOME */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faAngleDown, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot  } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faAngleDown, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot )
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+/* /.FONT AWESOME */
+
+
 Vue.config.productionTip = false
 
 new Vue({
