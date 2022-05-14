1dbc6db2030bfb91d523f0262c999e93fb508113
MyUsername <romamargine@gmail.com>
2022-05-14 14:56:13 +0200

    Add main-head

--- modern-schooling/src/main.js
+++ modern-schooling/src/main.js
@@ -4,14 +4,20 @@ import App from './App.vue'
 /* FONT AWESOME */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-import { faAngleDown, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot  } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faBook, faLifeRing, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot  } from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faAngleDown, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot )
+library.add(faAngleDown, faBook, faLifeRing, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-/* /.FONT AWESOME */
+
+/* FONT */
+import "@fontsource/poppins"
+import "@fontsource/poppins/100.css"
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/600.css"
+import "@fontsource/poppins/900.css"
 
 
 Vue.config.productionTip = false
