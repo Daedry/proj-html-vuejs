05b73a1cbb025a1bdc66e62c1cdf5c155f3dd696
MyUsername <romamargine@gmail.com>
2022-05-16 12:44:13 +0200

    Add main-info-counter

--- modern-schooling/src/App.vue
+++ modern-schooling/src/App.vue
@@ -274,6 +274,40 @@
         </div>
         <!-- /.container-md -->
       </section>
+      <!-- /.main-career-perspective -->
+
+      <sction class="main-info-counter">
+        <div class="container-lg">
+          <div class="container-md m-auto d-flex j-space-a">
+            <div class="finished-session info">
+              <div>1.926</div>
+              <h4>finished sessions</h4>
+            </div>
+            <!-- /.finished-session -->
+
+            <div class="enrolled-learners info">
+              <div>3.092+</div>
+              <h4>Enrolled learners</h4>
+            </div>
+            <!-- /.enrolled-learners -->
+
+            <div class="online-instructors info">
+              <div>200</div>
+              <h4>Online Instructors</h4>
+            </div>
+            <!-- /.online-instructors -->
+
+            <div class="satisfaction-rate info">
+              <div>100%</div>
+              <h4>satisfaction rate</h4>
+            </div>
+            <!-- /.satisfaction-rate -->
+          </div>
+          <!-- /.container-md -->
+        </div>
+        <!-- /container-lg -->
+      </sction>
+      <!-- /.info-counter -->
     </main>
   </div>
 </template>
@@ -366,7 +400,7 @@ header {
 }
 
 main {
-  height: 2000px;
+  height: 5000px;
   .main-head {
     background-color: $bg-main-head;
     overflow-x: hidden;
@@ -475,6 +509,7 @@ main {
   .main-career-perspective {
     .container-md {
       margin-top: 8rem;
+      margin-bottom: 3rem;
       .boxs {
         .box {
           .box-img {
@@ -531,7 +566,7 @@ main {
                     padding: 1rem;
                   }
                   .fa-circle-plus {
-                    color: #8C89A2;
+                    color: #8c89a2;
                   }
                   .fa-circle-minus {
                     color: $white;
@@ -547,7 +582,7 @@ main {
 
                 .text {
                   background-color: rgb(255, 255, 255);
-                  p{
+                  p {
                     padding: 1.45rem 1.75rem;
                     color: #6e6b69;
                     font-weight: 400;
@@ -556,6 +591,29 @@ main {
                 }
               }
             }
+          }
+        }
+      }
+    }
+  }
+
+  .main-info-counter {
+    .container-lg {
+      background-color: #faf8f6;
+      .container-md {
+        text-align: center;
+        .info {
+          padding: 2.5rem 0;
+          div {
+            font-size: 38px;
+            font-weight: 600;
+            color: $btn;
+          }
+          h4 {
+            color: #4e547e;
+            font-weight: 600;
+            font-size: 12px;
+            text-transform: uppercase;
           }
         }
       }
