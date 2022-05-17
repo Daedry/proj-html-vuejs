6796dda7e14207930940b7047db306d32d1c258f
MyUsername <romamargine@gmail.com>
2022-05-17 12:51:45 +0200

    Add footer

--- modern-schooling/src/main.js
+++ modern-schooling/src/main.js
@@ -1,28 +1,70 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
 /* FONT AWESOME */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-import { faAngleDown, faBook, faLifeRing, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot  } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowUpLong,
+  faAngleDown,
+  faBook,
+  faLifeRing,
+  faCircleUser,
+  faCartShopping,
+  faMagnifyingGlass,
+  faDownload,
+  faArrowRightLong,
+  faCircleMinus,
+  faCirclePlus,
+  faUser,
+  faCheck,
+  faFileLines,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faFacebookSquare,
+  faTwitter,
+  faInstagram,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 
-library.add(faAngleDown, faBook, faLifeRing, faCircleUser, faCartShopping, faMagnifyingGlass, faDownload, faArrowRightLong, faCircleMinus, faCirclePlus, faUser, faCheck, faFileLines, faLocationDot )
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(
+  faArrowUpLong,
+  faLinkedin,
+  faTwitter,
+  faInstagram,
+  faAngleDown,
+  faBook,
+  faFacebookSquare,
+  faLifeRing,
+  faCircleUser,
+  faCartShopping,
+  faMagnifyingGlass,
+  faDownload,
+  faArrowRightLong,
+  faCircleMinus,
+  faCirclePlus,
+  faUser,
+  faCheck,
+  faFileLines,
+  faLocationDot
+);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 /* FONT */
-import "@fontsource/poppins"
-import "@fontsource/poppins/100.css"
-import "@fontsource/poppins/500.css"
-import "@fontsource/poppins/600.css"
-import "@fontsource/poppins/700.css"
-import "@fontsource/poppins/900.css"
+import "@fontsource/poppins";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/900.css";
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
